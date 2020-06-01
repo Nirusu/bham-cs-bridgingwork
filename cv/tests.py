ec66aae5da44356aa471a5027aa2bba763733630
Nirusu <Nirusu@users.noreply.github.com>
2020-06-01 11:49:33 +0200

    First unit test

--- cv/tests.py
+++ cv/tests.py
@@ -1,3 +1,11 @@
+from django.urls import resolve
 from django.test import TestCase
+from .views import cv_show
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_cv_url_resolves_to_cv_homepage(self):
+        found = resolve('/cv/')
+        self.assertEqual(found.func, cv_show)
